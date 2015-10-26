238d82bb22d61530108b62a50d8c085606c1249d
Chris Nasser <chris.nasser@viacomcontractor.com>
2015-10-26 16:53:49 -0300

    Solved problem 5.

---
+++ 0005/optimized.rb
@@ -0,0 +1,6 @@
+# If n <= half / 2, then 2n is in the set, and n can be eliminated
+# Otherwise, n is not a factor of anything in the set, and must be considered
+
+high = ARGV[0].to_i
+low = high / 2 + 1
+puts (low .. high).reduce(:lcm)
